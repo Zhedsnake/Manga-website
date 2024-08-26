f678beb956e9e3f98ff6d3cada8e30b3d11a5bfb
Zhedsnake <besedinzena5@gmail.com>
2024-08-26 23:59:40 +0300

    Выделение определение соли в конструктор в UserService. Замена userToken на userId.

--- server/src/services/busines/UserService.ts
+++ server/src/services/busines/UserService.ts
@@ -4,85 +4,75 @@ import bcrypt from "bcrypt";
 
 
 class UserService {
+    private salt: string;
+
+    constructor() {
+        const salt: string | undefined = process.env.SALT;
+        if (!salt) {
+            throw new Error('JWT_SECRET is not defined');
+        }
+        this.salt = salt;
+    }
 
     async register(name: string, email: string, password: string) {
-        if (process.env.SALT) {
-            const saltRounds: number = parseInt(process.env.SALT);
-            const salt: string = await bcrypt.genSalt(saltRounds);
-            const hashedPassword: string = await bcrypt.hash(password, salt);
+        const saltRounds: number = parseInt(this.salt);
+        const salt: string = await bcrypt.genSalt(saltRounds);
+        const hashedPassword: string = await bcrypt.hash(password, salt);
 
-            const responseBD: { id: string } | {
-                error: string
-            } = await UserBDService.register(name, hashedPassword, email);
+        const responseBD: { id: string } | {
+            error: string
+        } = await UserBDService.register(name, hashedPassword, email);
 
-            if (responseBD) {
-                if ("error" in responseBD) {
-                    return responseBD
-                } else if ("id" in responseBD) {
-                    const payload = {
-                        user: {
-                            id: responseBD.id,
-                        },
-                    };
+        if ("id" in responseBD) {
+            const payload = {
+                user: {
+                    id: responseBD.id,
+                },
+            };
 
-                    const userToken = JwtService.getUserToken(payload);
-                    return {userToken: userToken};
-                }
-            }
-        } else if (!process.env.SALT) {
-            console.error(`Не указана соль`)
+            const userToken: string = JwtService.getUserToken(payload);
+
+            return {userToken: userToken};
+
+        } else if ("error" in responseBD) {
+            return responseBD
         }
     }
 
     async login(name: string, password: string) {
-        const responseBD: { error: string } | { name: string, id: string, password: string} | undefined = await UserBDService.login(name);
+        const responseBD:
+            { id: string,
+            name: string,
+            password: string }
+            | { error: string } = await UserBDService.login(name);
 
-        if (responseBD) {
-            if ("password" in responseBD) {
-                const isMatch: boolean = await bcrypt.compare(password, responseBD.password);
+        if ("password" in responseBD) {
+            const isMatch: boolean = await bcrypt.compare(password, responseBD.password);
 
-                if (!isMatch) {
-                    return {error: 'Имя или пароль неправельные'};
-                }
-
-                const payload = {
-                    user: {
-                        id: responseBD.id,
-                    },
-                };
-
-                const userToken = JwtService.getUserToken(payload);
-                return {userToken: userToken};
-
-            } else if ("error" in responseBD) {
-                return responseBD
+            if (!isMatch) {
+                return {error: 'Имя или пароль некорректны'};
             }
+
+            const payload = {
+                user: {
+                    id: responseBD.id,
+                },
+            };
+
+            const userToken = JwtService.getUserToken(payload);
+            return {userToken: userToken};
+
+        } else if ("error" in responseBD) {
+            return responseBD
         }
     }
 
-    async GetSmallUserInfoByToken(userToken: string) {
-        const userId: string | null = JwtService.decode(userToken);
-
-        if (!userId) {
-            return {error: "Токен устарел"}
-        }
-
+    async GetSmallUserInfoByToken(userId: string): Promise<{ name: string, pic: string } | undefined> {
         const userData: { name: string, pic: string } | undefined = await UserBDService.GetSmallUserInfoByToken(userId);
-
-        if (userData) {
-            return userData;
-        } else if (!userData) {
-            console.error("Неизвестная ошибка при GetSmallUserInfoByToken из бд")
-        }
+        return userData;
     }
 
-    async UpdateUserToken(Token: string) {
-        const userId: string | null = JwtService.decode(Token);
-
-        if (!userId) {
-            return {error: "Токен устарел"}
-        }
-
+    async UpdateUserToken(userId: string): Promise<{ userToken: string }> {
         const payload = {
             user: {
                 id: userId,
