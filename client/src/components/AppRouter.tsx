d1facd66c85c91febfde9a8ab0f68a5b72af451a
Zhedsnake <besedinzena5@gmail.com>
2024-08-27 14:12:57 +0300

    Добавление загрузки аутентификации в AppRouter

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -3,10 +3,15 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 import {privateRoutes, publicRoutes} from "../router";
 import {AuthContext, AuthContextType} from "../context";
+import Loader from "./UI/Loader/Loader.tsx";
 
 
 const AppRouter: React.FC = () => {
-    const {isUser} = useContext<AuthContextType>(AuthContext);
+    const {isUser, isAuthLoading} = useContext<AuthContextType>(AuthContext);
+
+    if (isAuthLoading) {
+        return <Loader/>
+    }
 
     return (
         isUser
