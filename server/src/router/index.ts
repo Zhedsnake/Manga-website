10240c53e45b454ad643845f59b4ba406018b5fb
Zhedsnake <besedinzena5@gmail.com>
2024-08-30 00:50:07 +0300

    Введение маршрутов для редактирования данных пользователя в index.

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -3,11 +3,13 @@ import imagesRouter from "./imagesRouters/imagesRouter";
 import guestRouter from "./guestRouter";
 import userRouter from "./userRouter";
 import authRouter from "./authRouter";
+import editUserRouter from "./editUserRouter";
 
 const routers = Router();
 
 routers.use('/api', authRouter);
 routers.use('/api', userRouter);
+routers.use('/api', editUserRouter);
 routers.use('/api', guestRouter);
 routers.use('/api', imagesRouter);
 
