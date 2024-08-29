cfdba88752b8941f6f7bfe988d62a7f018bddbc0
Zhedsnake <besedinzena5@gmail.com>
2024-08-30 00:49:01 +0300

    Перенос методов редактирования данных пользователя в EditUserInfoController.

--- server/src/router/editUserRouter.ts
+++ server/src/router/editUserRouter.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import UserControllers from "../controllers/UserControllers";
 import ProtectService from "../middlewares/ProtectService";
 import {upload} from "../middlewares/multer";
+import EditUserInfoController from "../controllers/EditUserInfoController";
 
 const editUserRouter = Router();
 
-editUserRouter.put('/edit-user-name-by-token', ProtectService.checkUserToken, UserControllers.EditUserNameByToken);
-editUserRouter.put('/edit-user-email-by-token', ProtectService.checkUserToken, UserControllers.EditUserEmailByToken);
-editUserRouter.put('/edit-user-password-by-token', ProtectService.checkUserToken, UserControllers.EditUserPasswordByToken);
-editUserRouter.put('/edit-user-avatar-by-token', ProtectService.checkUserToken, upload, UserControllers.EditUserAvatarByToken);
+editUserRouter.put('/edit-user-name-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserNameByToken);
+editUserRouter.put('/edit-user-email-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserEmailByToken);
+editUserRouter.put('/edit-user-password-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserPasswordByToken);
+editUserRouter.put('/edit-user-avatar-by-token', ProtectService.checkUserToken, upload, EditUserInfoController.EditUserAvatarByToken);
 
 export default editUserRouter;
