01650c32dec6dbc98ce34ddfe65f8834ed8909af
Zhedsnake <besedinzena5@gmail.com>
2024-08-29 17:19:37 +0300

    Очистка от лишнего

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,30 +1,8 @@
-import React, {useContext, useEffect} from 'react';
+import React from 'react';
 import MainContent from "../components/mainPage/MainContent.tsx";
 import LeftPanel from "../components/mainPage/LeftPanel.tsx";
-import {useActions} from "../hooks/useActions.ts";
-import {AuthContext, AuthContextType} from "../context";
 
 const Home: React.FC = () => {
-    const {
-        isUser
-    } = useContext<AuthContextType>(AuthContext);
-
-    const { updateUserToken } = useActions();
-
-    useEffect((): void => {
-        // getStuffsAction(page)
-        // }, [page]);
-    }, []);
-
-    useEffect(() => {
-        if (isUser) {
-            updateUserToken()
-        }
-
-        return () => {
-            // defStuffs()
-        };
-    }, []);
 
     return (
         <div className="container">
