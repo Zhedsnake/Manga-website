1da0c644860900c7a5e9441f45bd01afc4f0b1f1
Zhedsnake <besedinzena5@gmail.com>
2024-08-29 15:49:19 +0300

    Исправление типизации для смены имени пользователя

--- client/src/types/editUserInfo/nameForm.ts
+++ client/src/types/editUserInfo/nameForm.ts
@@ -9,18 +9,18 @@ export enum NameActionTypes {
     EDIT_NAME_ERROR = 'EDIT_NAME_ERROR',
     DEF_EDIT_NAME = 'DEF_EDIT_NAME'
 }
-interface GetNameAction {
+interface EditNameAction {
     type: NameActionTypes.EDIT_NAME;
 }
-interface GetNameSuccessAction {
+interface EditNameSuccessAction {
     type: NameActionTypes.EDIT_NAME_SUCCESS;
     payload: string
 }
-interface GetNameErrorAction {
+interface EditNameErrorAction {
     type: NameActionTypes.EDIT_NAME_ERROR;
     payload: string;
 }
 interface DefNameAction {
     type: NameActionTypes.DEF_EDIT_NAME;
 }
-export type NameAction = GetNameAction | GetNameSuccessAction | GetNameErrorAction | DefNameAction
+export type NameAction = EditNameAction | EditNameSuccessAction | EditNameErrorAction | DefNameAction
