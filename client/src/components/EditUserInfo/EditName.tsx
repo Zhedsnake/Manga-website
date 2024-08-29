74d0d64b4151a375c4021d242ba8f4046e221000
Zhedsnake <besedinzena5@gmail.com>
2024-08-29 15:48:30 +0300

    Компонент для работы с сменой никнейма пользователя

---
+++ client/src/components/EditUserInfo/EditName.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Loader from "../UI/Loader/Loader.tsx";
+import {useTypedSelector} from "../../hooks/useTypedSelector.ts";
+import {useActions} from "../../hooks/useActions.ts";
+
+const EditName : React.FC = () => {
+    const [name, setName] = useState<string>("")
+
+    const {loading: nameLoading, error: nameError} = useTypedSelector(state => state.nameForm);
+    const {editName} = useActions();
+
+    const handleEditName = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await editName(name);
+        setName("")
+    }
+
+    const handlerSetName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value)
+    }
+
+    return (
+        <>
+            {nameError && <div>{nameError}</div>}
+            {nameLoading
+                ? <Loader/>
+                :
+                <form role="form">
+                    <div className="mb-3">
+                        <label htmlFor="InputName">Никнейм</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="InputName"
+                            aria-describedby="emailHelp"
+                            value={name}
+                            onChange={handlerSetName}
+                        />
+                    </div>
+                    <button onClick={handleEditName} type="submit" className="btn btn-primary">Поменять
+                        никнейм
+                    </button>
+                </form>
+            }
+        </>
+    );
+};
+
+export default EditName;
