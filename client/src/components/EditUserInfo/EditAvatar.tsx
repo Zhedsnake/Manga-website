35357a178d89d517d3dfb332013d8ac157b98072
Zhedsnake <besedinzena5@gmail.com>
2024-08-30 13:55:50 +0300

    Валидация изображения на стороне клиента

--- client/src/components/EditUserInfo/EditAvatar.tsx
+++ client/src/components/EditUserInfo/EditAvatar.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useState} from 'react';
+import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
 import {useTypedSelector} from "../../hooks/useTypedSelector.ts";
 import {useActions} from "../../hooks/useActions.ts";
 import Loader from "../UI/Loader/Loader.tsx";
@@ -10,6 +10,7 @@ interface setMessageInterface {
 const EditAvatar: React.FC<setMessageInterface> = ({setMessage}) => {
     const [avatar, setAvatar] = useState<File | null>(null);
     const [coverPreview, setCoverPreview] = useState<string | null>(null);
+    const [avatarErrorState, setAvatarErrorState] = useState<string>("")
 
     const {loading: avatarLoading, error: avatarError} = useTypedSelector(state => state.avatarForm);
     const {editAvatar} = useActions();
@@ -28,19 +29,49 @@ const EditAvatar: React.FC<setMessageInterface> = ({setMessage}) => {
         e.preventDefault()
         setMessage("")
 
-        if (avatar) {
-            const imageFormData = new FormData();
-            imageFormData.append('avatar', avatar);
+        if (avatar && (avatar.type === "image/jpeg" || avatar.type === "image/png")) {
+            if (avatar.size > 368000) { // 3.68 MB in bytes
+                setAvatarErrorState("Размер файла не должен превышать 3.68 MB.");
+                return;
+            }
 
-            await editAvatar(imageFormData)
+            const img = new Image();
+            img.src = URL.createObjectURL(avatar);
+
+            img.onload = async () => {
+                if (img.width === img.height) {
+                    console.log(avatar);
+                    setAvatarErrorState("")
+                    const imageFormData = new FormData();
+                    imageFormData.append('avatar', avatar);
+
+                    await editAvatar(imageFormData)
+                } else {
+                    setAvatarErrorState("Изображение должно иметь соотношение сторон 1:1.");
+                }
+            };
+
+        } else if (!avatar){
+            setAvatarErrorState("Вы не загрузили изображение")
+            return;
+        } else {
+            setAvatarErrorState("Допустимые форматы jpeg или png")
+            return;
         }
 
-        setCoverPreview("")
+        setAvatar(null);
+        setCoverPreview(null);
     };
+
+    useEffect(() => {
+        if (avatarError) {
+            setAvatarErrorState(avatarError)
+        }
+    }, [avatarError]);
 
     return (
         <>
-            {avatarError && <div>{avatarError}</div>}
+            {avatarErrorState && <div>{avatarErrorState}</div>}
             {avatarLoading ? (
                 <Loader />
             ) : (
