5ae0cc26b3eacaf4381861afaa16e5fb465d2fcf
Zhedsnake <besedinzena5@gmail.com>
2024-09-02 15:48:43 +0300

    Исправление в getSmallUserInfoByTokenReducer

--- client/src/store/reducers/getSmallUserInfoByTokenReducer.ts
+++ client/src/store/reducers/getSmallUserInfoByTokenReducer.ts
@@ -15,23 +15,11 @@ const initialState: GetSmallUserInfoState = {
 export const getSmallUserInfoByTokenReducer = (state = initialState, action: GetSmallUserInfoAction): GetSmallUserInfoState => {
     switch (action.type) {
         case GetSmallUserInfoActionTypes.SMALL_USER_INFO:
-            return {
-                data: {
-                    name: "",
-                    pic: ""
-                },
-                loading: true,
-                error: null }
+            return { ...initialState, loading: true }
         case GetSmallUserInfoActionTypes.SMALL_USER_INFO_SUCCESS:
             return { data: action.payload, loading: false, error: null }
         case GetSmallUserInfoActionTypes.SMALL_USER_INFO_ERROR:
-            return {
-                data: {
-                    name: "",
-                    pic: ""
-                },
-                loading: false,
-                error: action.payload }
+            return { ...initialState, loading: false, error: action.payload }
         case GetSmallUserInfoActionTypes.DEF_SMALL_USER_INFO:
             return { ...initialState }
         default:
