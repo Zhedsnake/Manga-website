50aeca16940d890efd3e59d3fe6e46ec34a583cd
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 17:00:25 +0300

    Импорт handleRegister из кастомного хука для регистрации пользователя

--- client/src/components/Authentification/RegForm.tsx
+++ client/src/components/Authentification/RegForm.tsx
@@ -5,12 +5,15 @@ import NameInput from "./NameInput";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 import FormButton from "../UI/formButton/FormButton";
+import useHandleReg from "../../hooks/Auth/useHandleReg.ts";
 
 interface HandleRegisterType {
     handleRegister:(e: React.FormEvent) => Promise<void>;
 }
 
-const RegForm: React.FC<HandleRegisterType> = ({handleRegister}) => {
+const RegForm: React.FC<HandleRegisterType> = () => {
+
+    const handleRegister = useHandleReg()
 
     return (
         <FormAuth>
@@ -18,7 +21,7 @@ const RegForm: React.FC<HandleRegisterType> = ({handleRegister}) => {
             <NameInput />
             <EmailInput />
             <PasswordInput />
-            <FormButton onClick={handleRegister}>Зарегистрироваться</FormButton>
+            <FormButton onClick={handleRegister.handleRegister}>Зарегистрироваться</FormButton>
         </FormAuth>
     );
 };
