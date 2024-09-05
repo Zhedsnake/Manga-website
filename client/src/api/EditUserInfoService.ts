878e4a77dd6a81f1c75981f2666e6519295e2d48
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 16:52:29 +0300

    Исправление отлова ошибок от сервера на редактирование данных пользователя

--- client/src/api/EditUserInfoService.ts
+++ client/src/api/EditUserInfoService.ts
@@ -5,34 +5,50 @@ import {Tokens} from "../util/Tokens.ts";
 
 export default class EditUserInfoService {
     static async editNameRequest(name: string) {
-        const token = localStorage.getItem(Tokens.userToken);
-        axios.defaults.headers.common['user-token'] = token;
-        const response = await axios.put(`${API_URL}/edit-user-name-by-token`, {name});
-        return response;
+        try {
+            const token = localStorage.getItem(Tokens.userToken);
+            axios.defaults.headers.common['user-token'] = token;
+            const response = await axios.put(`${API_URL}/edit-user-name-by-token`, {name});
+            return response;
+        } catch (e) {
+            return {error: e.response.data.error};
+        }
     }
 
     static async editEmailRequest(email: string) {
-        const token = localStorage.getItem(Tokens.userToken);
-        axios.defaults.headers.common['user-token'] = token;
-        const response = await axios.put(`${API_URL}/edit-user-email-by-token`, {email});
-        return response;
+        try {
+            const token = localStorage.getItem(Tokens.userToken);
+            axios.defaults.headers.common['user-token'] = token;
+            const response = await axios.put(`${API_URL}/edit-user-email-by-token`, {email});
+            return response;
+        } catch (e) {
+            return {error: e.response.data.error};
+        }
     }
 
     static async editPasswordRequest(oldPassword: string, newPassword: string) {
-        const token = localStorage.getItem(Tokens.userToken);
-        axios.defaults.headers.common['user-token'] = token;
-        const response = await axios.put(`${API_URL}/edit-user-password-by-token`, {oldPassword, newPassword});
-        return response;
+        try {
+            const token = localStorage.getItem(Tokens.userToken);
+            axios.defaults.headers.common['user-token'] = token;
+            const response = await axios.put(`${API_URL}/edit-user-password-by-token`, {oldPassword, newPassword});
+            return response;
+        } catch (e) {
+            return {error: e.response.data.error};
+        }
     }
 
     static async editAvatarRequest(imageFormData) {
-        const token = localStorage.getItem(Tokens.userToken);
-        axios.defaults.headers.common['user-token'] = token;
-        const response = await axios.put(`${API_URL}/edit-user-avatar-by-token`, imageFormData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            }
-        });
-        return response;
+        try {
+            const token = localStorage.getItem(Tokens.userToken);
+            axios.defaults.headers.common['user-token'] = token;
+            const response = await axios.put(`${API_URL}/edit-user-avatar-by-token`, imageFormData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            });
+            return response;
+        } catch (e) {
+            return {error: e.response.data.error};
+        }
     }
 }
