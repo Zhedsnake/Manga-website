172f14a77492f6a6fe48ed3b7781c22e80aeaa2e
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 17:39:38 +0300

    Исправление вывода ошибок из midleware multer на сервере

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -16,11 +16,11 @@ const multerUpload = multer({ storage: storage });
 export const upload = (req: Request, res: Response, next: NextFunction) => {
   multerUpload.any()(req, res, (err) => {
     if (err) {
-      return res.status(400).send({message: 'Вы не выбрали файлы'});
+      return res.status(400).send({error: 'Вы не выбрали файлы'});
     }
 
     if (!req.files || (Array.isArray(req.files) && req.files.length === 0)) {
-      return res.status(400).send({message: 'Файлы не загружены'});
+      return res.status(400).send({error: 'Файлы не загружены'});
     }
 
     next();
