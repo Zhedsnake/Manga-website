11d5c7f15526fe0dc81429d93fa898058aaaf010
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 17:38:56 +0300

    Введение верификации для редактирования пароля пользователя

---
+++ client/src/util/Verification/EditUserInfo/verifyEditPassword.ts
@@ -0,0 +1,23 @@
+import verifyPassword from "../verifyPassword.ts";
+
+export default function verifyEditPassword(oldPassword: string, newPassword: string): {passwordError: string} | null {
+
+    if (!oldPassword) {
+        return {passwordError: "Не указан старый пароль"}
+    }
+
+    if (!newPassword) {
+        return {passwordError: "Не указан новый пароль"}
+    }
+
+    if (oldPassword === newPassword) {
+        return { passwordError: 'Новый пароль не должен совпадать со старым' };
+    }
+
+    const verificationResponse:{passwordError: string} | null | undefined = verifyPassword(newPassword)
+    if (verificationResponse) {
+        return verificationResponse;
+    }
+
+    return null
+}
