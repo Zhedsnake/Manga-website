bea1fa00626603d47ee307cb6c5e84244f6d633a
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 17:23:44 +0300

    Исправление action для редактирования пароля пользователя

--- client/src/store/action-creators/editUserInfo/passwordForm.ts
+++ client/src/store/action-creators/editUserInfo/passwordForm.ts
@@ -5,16 +5,18 @@ import {PasswordAction, PasswordActionTypes} from "../../../types/editUserInfo/p
 
 export const editPassword = (oldPassword: string, newPassword: string) => {
     return async (dispatch: Dispatch<PasswordAction>) => {
-        try {
             dispatch({type: PasswordActionTypes.EDIT_PASSWORD})
             const response = await EditUserInfoService.editPasswordRequest(oldPassword, newPassword)
+
+        if ("data" in response && "message" in response.data) {
             dispatch({type: PasswordActionTypes.EDIT_PASSWORD_SUCCESS, payload: response.data.message})
-        } catch (e) {
+        } else if ("error" in response) {
             dispatch({
                 type: PasswordActionTypes.EDIT_PASSWORD_ERROR,
-                payload: e.response.data.error
+                payload: response.error
             })
         }
+
     }
 }
 export const defEditPassword = () => {
