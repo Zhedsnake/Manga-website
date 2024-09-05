2422807e9f6dccc6dc794b55b928936fe0395e89
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 16:51:41 +0300

    Перенос контекстов в отдельные компоненты

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-
 //Redux
 import {Provider} from 'react-redux';
 import store from "./store";
@@ -7,56 +6,31 @@ import store from "./store";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import React, {useState} from "react";
-import {AuthContext} from "./context";
+import React from "react";
 import AppRouter from "./components/AppRouter";
-import {ToggleShowType} from "./types/AuthForm";
 import AuthUserGuestChecker from "./components/AuthUserGuestChecker.tsx";
 import UpdateUserToken from "./components/UpdateUserToken.tsx";
 import BrowserWebpCheck from "./components/BrowserWebpCheck.tsx";
 
+import ImportAuthContext from "./components/ImportAuthContext.tsx";
+import ImportEditUserInfoContext from "./components/ImportEditUserInfoContext.tsx";
+
 
 const App: React.FC = () => {
-    const [isAuth, setIsAuth] = useState<boolean>(false);
-    const [tokenOutdated, setTokenOutdated] = useState<boolean>(false)
-
-    const [isUser, setIsUser] = useState<boolean>(false);
-    const [isGuest, setIsGuest] = useState<boolean>(false);
-
-    const [isAuthLoading, setAuthLoading] = useState<boolean>(true);
-
-    const defToggleShowFormPasswords: ToggleShowType = {
-        toggleShowPassword: false,
-        toggleShowConfirmPassword: false
-    };
-    const [toggleShowFormPasswords, setToggleShowFormPasswords] = useState<ToggleShowType>({...defToggleShowFormPasswords});
-
 
     return (
         <Provider store={store}>
-            <AuthContext.Provider value={{
-                isAuth,
-                setIsAuth,
-                tokenOutdated,
-                setTokenOutdated,
-                isUser,
-                setIsUser,
-                isGuest,
-                setIsGuest,
-                isAuthLoading,
-                setAuthLoading,
-                toggleShowFormPasswords,
-                setToggleShowFormPasswords,
-                defToggleShowFormPasswords
-            }}>
-                <AuthUserGuestChecker>
-                    <UpdateUserToken>
-                        <BrowserWebpCheck>
-                            <AppRouter/>
-                        </BrowserWebpCheck>
-                    </UpdateUserToken>
-                </AuthUserGuestChecker>
-            </AuthContext.Provider>
+            <ImportAuthContext>
+                <ImportEditUserInfoContext>
+                    <AuthUserGuestChecker>
+                        <UpdateUserToken>
+                            <BrowserWebpCheck>
+                                <AppRouter/>
+                            </BrowserWebpCheck>
+                        </UpdateUserToken>
+                    </AuthUserGuestChecker>
+                </ImportEditUserInfoContext>
+            </ImportAuthContext>
         </Provider>
     )
 }
