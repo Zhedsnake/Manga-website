cfb516c0fc60145445ed5420d3a4c2c3888467b2
Zhedsnake <besedinzena5@gmail.com>
2024-09-05 17:38:27 +0300

    Введение верификации для редактирования никнейма пользователя пользователя

---
+++ client/src/util/Verification/EditUserInfo/verifyEditName.ts
@@ -0,0 +1,12 @@
+
+import verifyName from "../verifyName.ts";
+
+export default function verifyEditName(name: string): {nameError: string} | null {
+
+    const verificationResponse = verifyName(name)
+    if (verificationResponse) {
+        return verificationResponse
+    }
+
+    return null;
+}
