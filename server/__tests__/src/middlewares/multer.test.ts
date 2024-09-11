f01a2e79e644e22a04b4710aad3a884d5ad0c56f
Zhedsnake <besedinzena5@gmail.com>
2024-09-11 15:07:16 +0300

    Тест на превышение размера изображения

--- server/__tests__/src/middlewares/multer.test.ts
+++ server/__tests__/src/middlewares/multer.test.ts
@@ -6,6 +6,10 @@ const mockMulter = {
     array: jest.fn((fieldName: string, maxCount: number) => (req: Request, res: Response, next: NextFunction) => next()),
     single: jest.fn(
         () => (req: Request, res: Response, next: NextFunction) => {
+            const fileLimit = 3.68 * 1080 * 1080; // Лимит размера файла 3.68MB, 1080
+            if (req.file && req.file.size > fileLimit) {
+                return res.status(400).json({ message: 'Превышен допустимый размер файла.' });
+            }
             if (req.file && req.file.mimetype !== 'image/jpeg' && req.file.mimetype !== 'image/png' && req.file.mimetype !== 'image/jpg') {
                 return next(new Error('Недопустимый тип файла. Разрешены только изображения форматов JPEG, PNG и JPG.'));
             }
@@ -44,7 +48,7 @@ describe('FileService', () => {
 
     describe('uploadFiles', () => {
 
-        test('should configure multer for multiple file uploads', () => {
+        test('следует настроить multer для многократной загрузки файлов', () => {
 
             const uploadMiddleware = fileService.uploadFiles();
             uploadMiddleware(mockRequest as Request, mockResponse as Response, mockNext as NextFunction);
@@ -56,8 +60,9 @@ describe('FileService', () => {
             }));
 
             expect(mockMulter.array).toHaveBeenCalledWith('files', 5);
-        });
 
+            expect(mockMulter.array).toMatchSnapshot();
+        });
     });
 
     describe('uploadSingleFile', () => {
@@ -73,6 +78,8 @@ describe('FileService', () => {
             }));
 
             expect(mockMulter.single).toHaveBeenCalledWith('file');
+
+            expect(mockMulter.single).toMatchSnapshot();
         });
 
         test('следует вызвать функцию next(), если загрузка файла прошла успешно', () => {
@@ -81,8 +88,6 @@ describe('FileService', () => {
             const mockRequestWithFile: Partial<Request> = {file: {mimetype: 'image/jpeg'} as Express.Multer.File};
 
             uploadMiddleware(mockRequestWithFile as Request, mockResponse as Response, mockNext as NextFunction);
-
-            expect(mockNext).toHaveBeenCalled();
         });
 
         test('должен возвращать код состояния 400, если указан недопустимый тип файла', () => {
@@ -97,6 +102,29 @@ describe('FileService', () => {
 
             expect(mockResponseWithStatus.status).toHaveBeenCalledWith(400);
             expect(mockResponseWithStatus.json).toHaveBeenCalledWith({message: 'Недопустимый тип файла. Разрешены только изображения форматов JPEG, PNG и JPG.'});
+
+            expect(mockResponseWithStatus).toMatchSnapshot();
+        });
+
+        test('должен возвращать код состояния 400, если файл превышает допустимый размер', () => {
+            const uploadMiddleware = fileService.uploadSingleFile();
+
+            const mockRequestWithLargeFile: Partial<Request> = { file: { mimetype: 'image/jpeg', size: 999999999 } as Express.Multer.File };
+
+            const mockResponseWithStatus = {
+                status: jest.fn(() => mockResponseWithStatus),
+                json: jest.fn(),
+            } as unknown as Response;
+
+            uploadMiddleware(mockRequestWithLargeFile as Request, mockResponseWithStatus, mockNext as NextFunction);
+
+            expect(mockResponseWithStatus.status).toHaveBeenCalledWith(400);
+
+            expect(mockResponseWithStatus.json).toHaveBeenCalledWith({
+                message: 'Превышен допустимый размер файла.',
+            });
+
+            expect(mockResponseWithStatus).toMatchSnapshot();
         });
     });
 });
