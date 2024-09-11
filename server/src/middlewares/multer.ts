7505d06324f5afed4f436da00afe93ebf22789e6
Zhedsnake <besedinzena5@gmail.com>
2024-09-11 11:47:15 +0300

    Переработка multer

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,70 +1,59 @@
 import multer, { FileFilterCallback } from 'multer';
-import { Request } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-const FILE_SIZE = 1;
+const FILE_SIZE = 3.68; // 3.68MB
+const FULL_HD = 1080;
 
-export enum StorageType {
-  DISK = 'disk',
+
+class FileService {
+
+    private disk() {
+        return multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, '/tmp/buffer');
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.fieldname + '-' + Date.now());
+            },
+        });
+    }
+
+    private fileFilterConfig(req: Request, file: Express.Multer.File, cb: FileFilterCallback) {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+            cb(null, true);
+        } else {
+            cb(new Error('Недопустимый тип файла. Разрешены только изображения форматов JPEG, PNG и JPG.'));
+        }
+    }
+
+    uploadFiles() {
+
+        const upload = multer({
+            storage: this.disk(),
+            limits: { fileSize: FULL_HD * FULL_HD * FILE_SIZE },
+            fileFilter: this.fileFilterConfig,
+        });
+
+        return upload.array('files', 5);
+    }
+
+    uploadSingleFile() {
+
+        const upload = multer({
+            storage: this.disk(),
+            limits: { fileSize: FULL_HD * FULL_HD * FILE_SIZE },
+            fileFilter: this.fileFilterConfig,
+        });
+
+        return (req: Request, res: Response, next: NextFunction) => {
+            upload.single('file')(req, res, (err: any) => {
+                if (err) {
+                    return res.status(400).json({ message: err.message });
+                }
+                next();
+            });
+        };
+    }
 }
 
-export class FileService {
-
-  fileUtil = {
-    fileValidation(fn: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => void) {
-      return fn;
-    },
-  };
-
-  disk() {
-    return multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, '/tmp/my-uploads');
-      },
-      filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now());
-      },
-    });
-  }
-
-  uploadFiles = (storage: StorageType, validationFn: (request: Request, file: Express.Multer.File, cb: FileFilterCallback) => void) => {
-    const upload = multer({
-      storage: this[storage](),
-      limits: { fileSize: 1024 * 1024 * FILE_SIZE },
-      fileFilter: this.fileUtil.fileValidation(validationFn),
-    });
-
-    return upload.array('files', 5);
-  };
-}
-
-
-
-
-// import multer, { StorageEngine } from 'multer';
-// import { Request, Response, NextFunction } from 'express';
-//
-// const storage: StorageEngine = multer.diskStorage({
-//   filename: (
-//       req: Request,
-//       file: Express.Multer.File,
-//       cb: (error: Error | null, filename: string) => void
-//   ): void => {
-//     cb(null, file.originalname);
-//   }
-// });
-//
-// const multerUpload = multer({ storage: storage });
-//
-// export const upload = (req: Request, res: Response, next: NextFunction) => {
-//   multerUpload.any()(req, res, (err) => {
-//     if (err) {
-//       return res.status(400).send({error: 'Вы не выбрали файлы'});
-//     }
-//
-//     if (!req.files || (Array.isArray(req.files) && req.files.length === 0)) {
-//       return res.status(400).send({error: 'Файлы не загружены'});
-//     }
-//
-//     next();
-//   });
-// };
+export default new FileService();
--- server/src/router/editUserRouter.ts
+++ server/src/router/editUserRouter.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import ProtectService from "../middlewares/ProtectService";
-import {upload} from "../middlewares/multer";
 import EditUserInfoController from "../controllers/EditUserInfoController";
+import FileService from "../middlewares/multer";
 
 const editUserRouter = Router();
 
 editUserRouter.put('/edit-user-name-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserNameByToken);
 editUserRouter.put('/edit-user-email-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserEmailByToken);
 editUserRouter.put('/edit-user-password-by-token', ProtectService.checkUserToken, EditUserInfoController.EditUserPasswordByToken);
-editUserRouter.put('/edit-user-avatar-by-token', ProtectService.checkUserToken, upload, EditUserInfoController.EditUserAvatarByToken);
+
+editUserRouter.put(
+    '/edit-user-avatar-by-token',
+    ProtectService.checkUserToken,
+    FileService.uploadSingleFile,
+    EditUserInfoController.EditUserAvatarByToken
+);
 
 export default editUserRouter;
