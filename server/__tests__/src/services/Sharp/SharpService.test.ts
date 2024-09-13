df5c1ceb42e7e556401dc58627fc49e79344dc80
Zhedsnake <besedinzena5@gmail.com>
2024-09-13 14:29:41 +0300

    Тест для SharpService

--- server/__tests__/src/services/Sharp/SharpService.test.ts
+++ server/__tests__/src/services/Sharp/SharpService.test.ts
@@ -4,24 +4,28 @@ import SharpService from '../../../../src/services/Sharp/SharpService';
 import mockFs from 'mock-fs';
 
 // Мокаем sharp и его методы вручную
-const mockSharpInstance = {
-    webp: jest.fn().mockReturnThis(),
-    resize: jest.fn().mockReturnThis(),
-    toBuffer: jest.fn().mockResolvedValue(Buffer.from('mockedBuffer')),
-    toFile: jest.fn().mockResolvedValue({})
-};
-
-jest.mock('sharp', () => jest.fn(() => mockSharpInstance));
+jest.mock('sharp');
 
 describe('SharpService', () => {
+    const mockSharpInstance = {
+        webp: jest.fn().mockReturnThis(),
+        resize: jest.fn().mockReturnThis(),
+        toBuffer: jest.fn().mockResolvedValue(Buffer.from('mockedBuffer')),
+        toFile: jest.fn().mockResolvedValue({})
+    };
+
     beforeEach(() => {
+        jest.clearAllMocks();
+
+        (sharp as unknown as jest.Mock).mockReturnValue(mockSharpInstance);
+
         mockFs({
-            '/tmp': {} // создаем фиктивную файловую систему
+            '/tmp': {}
         });
     });
 
     afterEach(() => {
-        mockFs.restore(); // восстанавливаем файловую систему после тестов
+        mockFs.restore();
     });
 
     it('должен скомпилировать изображения в три формата и сохранить их', async () => {
@@ -46,11 +50,10 @@ describe('SharpService', () => {
             minimizedWebpFilePath: '/tmp/test-480p.webp',
         });
 
-        // Проверяем, что методы sharp были вызваны корректное количество раз
-        expect(sharp).toHaveBeenCalledTimes(3); // вызовы sharp()
-        expect(mockSharpInstance.webp).toHaveBeenCalledTimes(2); // вызовы .webp()
-        expect(mockSharpInstance.resize).toHaveBeenCalledTimes(1); // вызовы .resize()
-        expect(mockSharpInstance.toBuffer).toHaveBeenCalledTimes(2); // вызовы .toBuffer()
-        expect(mockSharpInstance.toFile).toHaveBeenCalledTimes(3); // вызовы .toFile()
+        expect(sharp).toHaveBeenCalledTimes(5);
+        expect(mockSharpInstance.webp).toHaveBeenCalledTimes(2);
+        expect(mockSharpInstance.resize).toHaveBeenCalledTimes(1);
+        expect(mockSharpInstance.toBuffer).toHaveBeenCalledTimes(2);
+        expect(mockSharpInstance.toFile).toHaveBeenCalledTimes(3);
     });
 });
