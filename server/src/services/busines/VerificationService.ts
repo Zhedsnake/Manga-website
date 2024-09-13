db6191d2234558d0886b36ee1383842b94d39422
Zhedsnake <besedinzena5@gmail.com>
2024-09-13 15:23:03 +0300

    Исправление в VerificationService

--- server/src/services/busines/VerificationService.ts
+++ server/src/services/busines/VerificationService.ts
@@ -223,27 +223,13 @@ class VerificationService {
         return null
     }
 
-    async VerifyEditAvatar(avatarFile: UploadedImageByMulter[]): Promise<{ error: string } | null> {
+    async VerifyEditAvatar(avatarFile: UploadedImageByMulter): Promise<{ error: string } | null> {
 
-        if (!avatarFile || avatarFile.length === 0) {
-            return { error: 'Вы не загрузили аватарку' };
-        }
-
-        if (avatarFile.length > 1) {
-            return { error: 'Вы загрузили больше одного файла' };
-        }
-
-        const avatarFileLocal = avatarFile[0];
-
-        if (avatarFileLocal.mimetype !== "image/jpeg" && avatarFileLocal.mimetype !== "image/png" && avatarFileLocal.mimetype !== "image/jpg") {
+        if (avatarFile.mimetype !== "image/jpeg" && avatarFile.mimetype !== "image/png" && avatarFile.mimetype !== "image/jpg") {
             return { error: 'Изображение должно быть в формате jpg или png' };
         }
 
-        if (avatarFileLocal.size > 368000) { // 3.68 MB in bytes
-            return { error: "Размер файла не должен превышать 3.68 MB." };
-        }
-
-        const metadata = await sharp(avatarFileLocal.path).metadata();
+        const metadata = await sharp(avatarFile.path).metadata();
 
         if (metadata.width !== metadata.height) {
             return { error: 'Изображение должно иметь соотношение сторон 1:1.' };
