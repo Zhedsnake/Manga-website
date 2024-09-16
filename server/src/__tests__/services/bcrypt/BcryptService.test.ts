e5871d8736b4e18d0ca46327b788d81cc1497073
Zhedsnake <besedinzena5@gmail.com>
2024-09-16 17:23:27 +0300

    Тест BcryptService

---
+++ server/src/__tests__/services/bcrypt/BcryptService.test.ts
@@ -0,0 +1,30 @@
+import {BcryptService} from '../../../services/bcrypt/BcryptService';
+
+describe('BcryptService', () => {
+    let bcryptService: BcryptService;
+
+    beforeAll(async () => {
+        bcryptService = new BcryptService();
+        await bcryptService['initSalt']();
+    });
+
+    test('должно правильно хэшировать и сравнивать значение', async () => {
+        const value = 'mySecretValue';
+
+        const hashedValue = await bcryptService.Hash(value);
+        expect(hashedValue).not.toBe(value);
+
+        const isMatch = await bcryptService.Compare(value, hashedValue);
+        expect(isMatch).toBe(true);
+    });
+
+    test('должно сравнивать разные значения и возвращать false', async () => {
+        const value = 'mySecretValue';
+        const differentValue = 'anotherValue';
+
+        const hashedValue = await bcryptService.Hash(value);
+
+        const isMatch = await bcryptService.Compare(differentValue, hashedValue);
+        expect(isMatch).toBe(false);
+    });
+});
