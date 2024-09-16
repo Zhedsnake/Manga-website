2360a8c3af7524ad761135a8bcc41243e17849b2
Zhedsnake <besedinzena5@gmail.com>
2024-09-16 16:04:25 +0300

    Исправление теста SharpService

--- server/src/__tests__/services/Sharp/SharpService.test.ts
+++ server/src/__tests__/services/Sharp/SharpService.test.ts
@@ -1,11 +1,8 @@
-import sharp from 'sharp';
 import { UploadedImageByMulter } from '../../../types/uploadedImageByMulter';
 import SharpService from '../../../services/Sharp/SharpService';
 import mockFs from 'mock-fs';
 import fs from "fs";
-import * as path from "node:path";
-
-jest.mock('sharp');
+import * as path from 'path';
 
 describe('SharpService', () => {
 
@@ -15,7 +12,8 @@ describe('SharpService', () => {
         mockFs({
             '/tmp': {},
             'src/__tests__/images': {
-                '1x1.png': Buffer.from([0, 0, 0, 0])
+                '1x1.png': fs.readFileSync(path.resolve(__dirname, '../../images/1x1.png')),
+                'noSymImg.png': fs.readFileSync(path.resolve(__dirname, '../../images/noSymImg.png'))
             }
         });
     });
@@ -26,52 +24,38 @@ describe('SharpService', () => {
 
 
     describe('CompileImageInThreeFormats', () => {
-        const mockSharpInstance = {
-            webp: jest.fn().mockReturnThis(),
-            resize: jest.fn().mockReturnThis(),
-            toBuffer: jest.fn().mockResolvedValue(Buffer.from('mockedBuffer')),
-            toFile: jest.fn().mockResolvedValue({})
-        };
 
         test('должен скомпилировать изображения в три формата и сохранить их', async () => {
+            const imagePath = path.resolve(__dirname, '../../images/1x1.png');
 
-            (sharp as unknown as jest.Mock).mockReturnValue(mockSharpInstance);
-
-            const imageBuffer = 'fakeImageBuffer';
             const avatarFile: UploadedImageByMulter = {
                 fieldname: 'avatar',
                 originalname: 'test.png',
                 encoding: '7bit',
                 mimetype: 'image/png',
-                buffer: Buffer.from(imageBuffer),
-                size: 1024,
+                buffer: fs.readFileSync(imagePath),
+                size: fs.statSync(imagePath).size,
                 filename: 'test.png',
-                path: '/tmp/test.png',
-                destination: '/tmp'
+                path: 'C:/temp/test.png',
+                destination: 'C:/temp'
             };
 
-            const result = await SharpService.CompileImageInThreeFormats(imageBuffer, avatarFile);
+            const result = await SharpService.CompileImageInThreeFormats(imagePath, avatarFile);
 
             expect(result).toEqual({
-                webpFilePath: '/tmp/test.webp',
-                minimizedFilePath: '/tmp/test-480p.png',
-                minimizedWebpFilePath: '/tmp/test-480p.webp',
+                minimizedFilePath: "/tmp/test-480p.png",
+                minimizedWebpFilePath: "/tmp/test-480p.webp",
+                webpFilePath: "/tmp/test.webp",
             });
-
-            expect(sharp).toHaveBeenCalledTimes(5);
-            expect(mockSharpInstance.webp).toHaveBeenCalledTimes(2);
-            expect(mockSharpInstance.resize).toHaveBeenCalledTimes(1);
-            expect(mockSharpInstance.toBuffer).toHaveBeenCalledTimes(2);
-            expect(mockSharpInstance.toFile).toHaveBeenCalledTimes(3);
         });
     });
 
     describe('GetWidthAndHeight', () => {
 
-        test('должен вернуть ширину и высоту изображения', async () => {
-            const imagePath = path.resolve(__dirname, '../../images/1x1.png');
+        test('должен вернуть ширину 500 и высоту 500 изображения', async () => {
+            const imageSymPath = path.resolve(__dirname, '../../images/1x1.png');
 
-            expect(fs.existsSync(imagePath)).toBe(true);
+            expect(fs.existsSync(imageSymPath)).toBe(true);
 
             jest.unmock('sharp');
 
@@ -80,10 +64,10 @@ describe('SharpService', () => {
                 originalname: '1x1.png',
                 encoding: '7bit',
                 mimetype: 'image/png',
-                buffer: fs.readFileSync(imagePath),
-                size: fs.statSync(imagePath).size,
+                buffer: fs.readFileSync(imageSymPath),
+                size: fs.statSync(imageSymPath).size,
                 filename: '1x1.png',
-                path: imagePath,
+                path: imageSymPath,
                 destination: '/tmp'
             };
 
@@ -91,11 +75,36 @@ describe('SharpService', () => {
 
             expect(result).toHaveProperty('width');
             expect(result).toHaveProperty('height');
-            //
-            // console.log(`Width: ${result.width}, Height: ${result.height}`);
 
-            // Можно добавить проверку на конкретные значения ширины и высоты, если известно заранее
-            // expect(result).toEqual({ width: 800, height: 600 });
+            expect(result).toEqual({ width: 500, height: 500 }); 
+        });
+
+
+        test('должен вернуть ширину и высоту изображения', async () => {
+            const imageNoSymPath = path.resolve(__dirname, '../../images/noSymImg.png');
+
+            expect(fs.existsSync(imageNoSymPath)).toBe(true);
+
+            jest.unmock('sharp');
+
+            const avatarFile: UploadedImageByMulter = {
+                fieldname: 'avatar',
+                originalname: 'noSymImg.png',
+                encoding: '7bit',
+                mimetype: 'image/png',
+                buffer: fs.readFileSync(imageNoSymPath),
+                size: fs.statSync(imageNoSymPath).size,
+                filename: 'noSymImg.png',
+                path: imageNoSymPath,
+                destination: '/tmp'
+            };
+
+            const result = await SharpService.GetWidthAndHeight(avatarFile);
+
+            expect(result).toHaveProperty('width');
+            expect(result).toHaveProperty('height');
+
+            expect(result).toEqual({ width: 2650, height: 1600 });
         });
     });
 });
