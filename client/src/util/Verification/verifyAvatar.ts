ab44aae93fdaad7036297626035893efe144b87f
Zhedsnake <besedinzena5@gmail.com>
2024-09-20 12:54:45 +0300

    Исправление verifyAvatar

--- client/src/util/Verification/verifyAvatar.ts
+++ client/src/util/Verification/verifyAvatar.ts
@@ -1,27 +1,27 @@
-
-export default function verifyAvatar(avatar: File | null): {avatarError: string} | null {
-
+export default async function verifyAvatar(avatar: File | null): Promise<{ avatarError: string } | null> {
     if (!avatar) {
-        return {avatarError: "Вы не загрузили аватарку"};
+        return { avatarError: "Вы не загрузили аватарку" };
     }
 
     if (avatar.size > 368000) { // 3.68 MB in bytes
-        return {avatarError: "Размер файла не должен превышать 3.68 MB."};
+        return { avatarError: "Размер файла не должен превышать 3.68 MB." };
     }
 
     if (avatar.type !== "image/jpeg" && avatar.type !== "image/png" && avatar.type !== "image/jpg") {
-        return {avatarError: "Допустимые форматы jpeg, jpg или png"};
+        return { avatarError: "Допустимые форматы jpeg, jpg или png" };
     }
 
     const img = new Image();
     img.src = URL.createObjectURL(avatar);
 
-    img.onload = async () => {
-        if (img.width !== img.height) {
+    return new Promise((resolve) => {
+        img.onload = () => {
 
-            return {avatarError: "Изображение должно иметь соотношение сторон 1:1."};
-        }
-    };
-
-    return null;
+            if (img.width !== img.height) {
+                resolve({ avatarError: "Изображение должно иметь соотношение сторон 1:1." });
+            } else {
+                resolve(null);
+            }
+        };
+    });
 }
