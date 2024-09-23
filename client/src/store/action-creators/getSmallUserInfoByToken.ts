9c2bafe741291e0bbdc8c001ed7a7231c15afa14
Zhedsnake <besedinzena5@gmail.com>
2024-09-23 13:50:47 +0300

    Исправление getSmallUserInfoByToken

--- client/src/store/action-creators/getSmallUserInfoByToken.ts
+++ client/src/store/action-creators/getSmallUserInfoByToken.ts
@@ -3,24 +3,35 @@ import {
     GetSmallUserInfoAction, GetSmallUserInfoActionTypes,
 } from "../../types/getSmallUserInfo.ts";
 import UserService from "../../api/UserService.ts";
-import {AxiosResponse} from "axios";
-
+import {AxiosError, AxiosResponse} from "axios";
 
 export const getSmallUserInfoByToken = () => {
     return async (dispatch: Dispatch<GetSmallUserInfoAction>) => {
         try {
-            dispatch({type: GetSmallUserInfoActionTypes.SMALL_USER_INFO})
+            dispatch({type: GetSmallUserInfoActionTypes.SMALL_USER_INFO});
             const response: AxiosResponse<{ name: string, pic: string } | { name: string, minPicWebp: string }> = await UserService.getSmallUserInfoByToken();
 
-            if ( "data" in response ) {
-                dispatch({type: GetSmallUserInfoActionTypes.SMALL_USER_INFO_SUCCESS, payload: response.data})
+            if ("data" in response) {
+                dispatch({type: GetSmallUserInfoActionTypes.SMALL_USER_INFO_SUCCESS, payload: response.data});
             }
         } catch (e) {
-            if ( "error" in e.response.data) {
+            if (e instanceof AxiosError) {
+                if (e.response?.data && "error" in e.response.data) {
+                    dispatch({
+                        type: GetSmallUserInfoActionTypes.SMALL_USER_INFO_ERROR,
+                        payload: e.response.data.error
+                    });
+                } else {
+                    dispatch({
+                        type: GetSmallUserInfoActionTypes.SMALL_USER_INFO_ERROR,
+                        payload: "Неизвестная ошибка"
+                    });
+                }
+            } else {
                 dispatch({
                     type: GetSmallUserInfoActionTypes.SMALL_USER_INFO_ERROR,
-                    payload: e.response.data.error
-                })
+                    payload: "Ошибка выполнения запроса"
+                });
             }
         }
     }
@@ -28,6 +39,6 @@ export const getSmallUserInfoByToken = () => {
 
 export const defSmallUserInfoByToken = () => {
     return async (dispatch: Dispatch<GetSmallUserInfoAction>) => {
-        dispatch({type: GetSmallUserInfoActionTypes.DEF_SMALL_USER_INFO})
+        dispatch({type: GetSmallUserInfoActionTypes.DEF_SMALL_USER_INFO});
     }
 }
