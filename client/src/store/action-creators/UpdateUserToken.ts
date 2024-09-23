fb43da353feb00bf45a89bb896d9e853e4de0b03
Zhedsnake <besedinzena5@gmail.com>
2024-09-23 13:49:37 +0300

    Исправление updateUserToken

--- client/src/store/action-creators/UpdateUserToken.ts
+++ client/src/store/action-creators/UpdateUserToken.ts
@@ -1,23 +1,35 @@
 import {Dispatch} from "react";
 import UserService from "../../api/UserService.ts";
 import {UpdateUserTokenAction, UpdateUserTokenActionTypes} from "../../types/updateUserToken.ts";
-
+import {AxiosError} from "axios";
 
 export const updateUserToken = () => {
     return async (dispatch: Dispatch<UpdateUserTokenAction>) => {
         try {
-            dispatch({type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN})
+            dispatch({type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN});
             const response = await UserService.updateUserToken();
 
-            if ( "userToken" in response.data) {
-                dispatch({type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN_SUCCESS, payload: response.data.userToken})
+            if ("userToken" in response.data) {
+                dispatch({type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN_SUCCESS, payload: response.data.userToken});
             }
         } catch (e) {
-            if ( "error" in e.response.data) {
+            if (e instanceof AxiosError) {
+                if (e.response?.data && "error" in e.response.data) {
+                    dispatch({
+                        type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN_ERROR,
+                        payload: e.response.data.error
+                    });
+                } else {
+                    dispatch({
+                        type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN_ERROR,
+                        payload: "Неизвестная ошибка"
+                    });
+                }
+            } else {
                 dispatch({
                     type: UpdateUserTokenActionTypes.UPDATE_USER_TOKEN_ERROR,
-                    payload: e.response.data.error
-                })
+                    payload: "Ошибка выполнения запроса"
+                });
             }
         }
     }
@@ -25,6 +37,6 @@ export const updateUserToken = () => {
 
 export const defUpdateUserToken = () => {
     return async (dispatch: Dispatch<UpdateUserTokenAction>) => {
-        dispatch({type: UpdateUserTokenActionTypes.DEF_UPDATE_USER_TOKEN})
+        dispatch({type: UpdateUserTokenActionTypes.DEF_UPDATE_USER_TOKEN});
     }
 }
