5f8be8331bd0613984cc4ab3df28ed5c9b4fef98
Zhedsnake <besedinzena5@gmail.com>
2024-09-25 17:31:02 +0300

    тестирование logInReducer

---
+++ client/src/__tests__/unit/store/reducers/logIn-Registration/logInReducer.test.ts
@@ -0,0 +1,77 @@
+import { logInReducer } from "../../../../../store/reducers/logIn-Registration/logInReducer";
+import { LogInActionTypes, LogInAction, LogInState } from "../../../../../types/logInRegistration/logIn";
+
+describe('logInReducer', () => {
+    const initialState: LogInState = {
+        logInToken: '',
+        logInLoading: false,
+        logInError: '',
+    };
+
+    test('должен возвращать начальное состояние по умолчанию', () => {
+        const newState = logInReducer(undefined, {} as LogInAction);
+        expect(newState).toEqual(initialState);
+    });
+
+    test('должен обрабатывать LOG_IN', () => {
+        const action: LogInAction = {
+            type: LogInActionTypes.LOG_IN,
+        };
+
+        const expectedState: LogInState = {
+            logInToken: '',
+            logInLoading: true,
+            logInError: ''
+        };
+
+        const newState = logInReducer(initialState, action);
+        expect(newState).toEqual(expectedState);
+    });
+
+    test('должен обрабатывать LOG_IN_SUCCESS', () => {
+        const action: LogInAction = {
+            type: LogInActionTypes.LOG_IN_SUCCESS,
+            payload: "token_123"
+        };
+
+        const expectedState: LogInState = {
+            logInToken: "token_123",
+            logInLoading: false,
+            logInError: ''
+        };
+
+        const newState = logInReducer(initialState, action);
+        expect(newState).toEqual(expectedState);
+    });
+
+    test('должен обрабатывать LOG_IN_ERROR', () => {
+        const action: LogInAction = {
+            type: LogInActionTypes.LOG_IN_ERROR,
+            payload: "Invalid credentials"
+        };
+
+        const expectedState: LogInState = {
+            logInToken: '',
+            logInLoading: false,
+            logInError: "Invalid credentials"
+        };
+
+        const newState = logInReducer(initialState, action);
+        expect(newState).toEqual(expectedState);
+    });
+
+    test('должен обрабатывать DEF_LOG_IN и сбрасывать состояние', () => {
+        const action: LogInAction = {
+            type: LogInActionTypes.DEF_LOG_IN,
+        };
+
+        const currentState: LogInState = {
+            logInToken: "token_123",
+            logInLoading: true,
+            logInError: "Some error"
+        };
+
+        const newState = logInReducer(currentState, action);
+        expect(newState).toEqual(initialState);
+    });
+});
