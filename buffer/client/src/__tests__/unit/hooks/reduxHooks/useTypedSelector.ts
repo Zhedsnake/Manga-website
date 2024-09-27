5419279c6feaba2245dd29c5b72e016b1248f296
Zhedsnake <besedinzena5@gmail.com>
2024-09-27 18:05:33 +0300

    Перенос кастомных хуков для работы с типищированными редьюсерами на Подумать

---
+++ buffer/client/src/__tests__/unit/hooks/reduxHooks/useTypedSelector.ts
@@ -0,0 +1,5 @@
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {RootState} from "../../../../../../../client/src/store/reducers";
+
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
