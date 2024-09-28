360fa0ebcaf1a93628f7735e6dc6c03c9f622424
Zhedsnake <besedinzena5@gmail.com>
2024-09-28 11:26:27 +0300

    Тестирования nameFormSlice

---
+++ client/src/__tests__/unit/store/reducers/editUserInfo/nameFormSlice.test.ts
@@ -0,0 +1,71 @@
+import nameFormReducer, { defEditName } from '../../../../../store/reducers/editUserInfo/nameFormSlice';
+import { editName } from '../../../../../store/action-creators/editUserInfo/nameForm';
+import { EditNameState } from '../../../../../types/editUserInfo/nameForm';
+
+jest.mock('../../../../../api/EditUserInfoService', () => ({
+    __esModule: true,
+    default: {
+        editEmailRequest: jest.fn(),
+        editNameRequest: jest.fn(),
+        editPasswordRequest: jest.fn(),
+        editAvatarRequest: jest.fn(),
+    },
+}));
+
+describe('nameFormSlice reducer tests', () => {
+
+    const initialState: EditNameState = {
+        message: "",
+        loading: false,
+        error: null
+    };
+
+    test('должно быть возвращено исходное состояние', () => {
+        expect(nameFormReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+    });
+
+    test('должен обрабатывать действие defEditName', () => {
+        const newState = nameFormReducer(initialState, defEditName());
+        expect(newState).toEqual({
+            message: "",
+            loading: false,
+            error: null
+        });
+    });
+
+    test('должен обрабатывать действие editName.pending', () => {
+        const action = { type: editName.pending.type };
+        const newState = nameFormReducer(initialState, action);
+        expect(newState).toEqual({
+            message: "",
+            loading: true,
+            error: null
+        });
+    });
+
+    test('должен обрабатывать действие editName.fulfilled', () => {
+        const action = {
+            type: editName.fulfilled.type,
+            payload: 'Name updated successfully'
+        };
+        const newState = nameFormReducer(initialState, action);
+        expect(newState).toEqual({
+            message: 'Name updated successfully',
+            loading: false,
+            error: null
+        });
+    });
+
+    test('должен обрабатывать действие editName.rejected', () => {
+        const action = {
+            type: editName.rejected.type,
+            payload: 'Name update failed'
+        };
+        const newState = nameFormReducer(initialState, action);
+        expect(newState).toEqual({
+            message: "",
+            loading: false,
+            error: 'Name update failed'
+        });
+    });
+});
