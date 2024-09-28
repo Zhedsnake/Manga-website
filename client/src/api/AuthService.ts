7cddf15538efdf4f9985bcb1f7afb9e5cb6d28f1
Zhedsnake <besedinzena5@gmail.com>
2024-09-28 13:15:36 +0300

    Исправление AuthService

--- client/src/api/AuthService.ts
+++ client/src/api/AuthService.ts
@@ -1,15 +1,25 @@
-import axios from 'axios';
+import axios, {AxiosError} from 'axios';
 import {API_URL} from './config';
 
 
 export default class AuthService {
     static async logInRequest(name: string, password: string) {
-        const response = await axios.post(`${API_URL}/login`, {name, password});
-        return response;
+        try {
+            const response = await axios.post(`${API_URL}/login`, {name, password});
+            return response;
+        } catch (e) {
+            const error = e as AxiosError<{ error: string }>;
+            throw new Error(error.response?.data?.error || "Неизвестная ошибка");
+        }
     }
 
     static async registerRequest(name: string, email: string, password: string) {
-        const response = await axios.post(`${API_URL}/register`, {name, email, password});
-        return response;
+        try {
+            const response = await axios.post(`${API_URL}/register`, {name, email, password});
+            return response;
+        } catch (e) {
+            const error = e as AxiosError<{ error: string }>;
+            throw new Error(error.response?.data?.error || "Неизвестная ошибка");
+        }
     }
 }
