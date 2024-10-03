87bfcdbd37b1c53581634deb852367745f22f80d
Zhedsnake <besedinzena5@gmail.com>
2024-10-03 12:06:24 +0300

    Набросок интеграционного теста GuestService

---
+++ client/src/__tests__/integration/api/GuestService.test.ts
@@ -0,0 +1,12 @@
+import GuestService from '../../../api/GuestService';
+
+describe('GuestService (Интеграция)', () => {
+    
+    test('должен вернуть токен гостя при успешной регистрации', async () => {
+        const response = await GuestService.registerGuest();
+
+        expect(response.status).toBe(201); // Проверяем успешный статус
+        expect(response.data).toHaveProperty('guestToken'); // Проверяем наличие токена
+        expect(typeof response.data.guestToken).toBe('string'); // Проверяем тип токена
+    });
+});
