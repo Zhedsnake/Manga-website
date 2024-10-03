ec7b8489b2261ec10bfab1328488a39186ee1bae
Zhedsnake <besedinzena5@gmail.com>
2024-10-03 12:20:41 +0300

    Интеграция редьюсеров слайсов в rootReducer

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,9 +1,35 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import emailFormReducer from "./editUserInfo/emailFormSlice.ts";
+import LogInSlice from "./logIn-Registration/logInSlice.ts";
+import registrationSlice from "./logIn-Registration/registrationSlice.ts";
+import avatarFormSlice from "./editUserInfo/avatarFormSlice.ts";
+import nameFormSlice from "./editUserInfo/nameFormSlice.ts";
+import passwordFormSlice from "./editUserInfo/passwordFormSlice.ts";
+import authFormSlice from "./authForm/authFormSlice.ts";
+import authFormErrorSlice from "./authForm/authFormErrorSlice.ts";
+
+import getGuestTokenSlice from "./getGuestTokenSlice.ts";
+import getSmallUserInfoByTokenSlice from "./getSmallUserInfoByTokenSlice.ts";
+import updateUserTokenSlice from "./updateUserTokenSlice.ts";
+import getUserInfoByTokenSlice from "./getUserInfoByTokenSlice.ts";
 
 
 export const rootReducer = combineReducers({
+    logIn: LogInSlice,
+    registration: registrationSlice,
+
+    avatarForm: avatarFormSlice,
+    nameForm: nameFormSlice,
     emailForm: emailFormReducer,
+    passwordForm: passwordFormSlice,
+
+    authForm: authFormSlice,
+    authFormError: authFormErrorSlice,
+
+    getGuestToken: getGuestTokenSlice,
+    getSmallUserInfoByToken: getSmallUserInfoByTokenSlice,
+    updateUserToken: updateUserTokenSlice,
+    getUserInfoByToken: getUserInfoByTokenSlice,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
