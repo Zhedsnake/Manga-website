a6a78b672ee8b157c724e6231ffccfe22a81e818
Zhedsnake <besedinzena5@gmail.com>
2024-10-03 12:21:07 +0300

    Исправление store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,15 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {postAPI} from "../services/PostService";
+// import {postAPI} from "../services/PostService";
 import {rootReducer} from "./reducers";
 
-export const setupStore = () => {
+const setupStore = () => {
     return configureStore({
         reducer: rootReducer,
         middleware: (getDefaultMiddleware) =>
             getDefaultMiddleware()
-                .concat(postAPI.middleware)
+                // .concat(postAPI.middleware)
     })
 }
 
+export default setupStore;
 export type AppStore = ReturnType<typeof setupStore>
 export type AppDispatch = AppStore['dispatch']
