3f9173f0354baf27d2b7593092fd37d1d756dece
Zhedsnake <besedinzena5@gmail.com>
2024-10-08 15:11:27 +0300

    Исправление и импорт redux toolkit в useHandleEditEmail

--- client/src/hooks/EditUserInfoHooks/useHandleEditEmail.ts
+++ client/src/hooks/EditUserInfoHooks/useHandleEditEmail.ts
@@ -1,17 +1,16 @@
 import React, {useContext, useEffect, useState} from "react";
-import {useTypedSelector} from "../../../../buffer/client/src/hooks/reduxHooks/useTypedSelector.ts";
-import {useActions} from "../../../../buffer/client/src/hooks/reduxHooks/useActions.ts";
+import {useActions} from "../useActions";
 import verifyEmail from "../../util/Verification/verifyEmail.ts";
 import {EditUserInfoContext, EditUserInfoContextType} from "../../contexts/EditUserInfoContext.ts";
+import {useAppSelector} from "../reduxHooks-toolkit/useRedux.ts";
 
 export default function useHandleEditEmail(email: string, clear: () => void) {
     const { setMessage } = useContext<EditUserInfoContextType>(EditUserInfoContext);
 
     const [error, setError] = useState<string>("");
 
-    //! Патом переделать под redux toolkit
-
-    const { loading: emailLoading, error: emailError } = useTypedSelector(state => state.emailForm);
+    // const { loading: emailLoading, error: emailError } = useTypedSelector(state => state.emailForm);
+    const { loading: emailLoading, error: emailError } = useAppSelector(state => state.emailForm);
     const { editEmail } = useActions();
 
     const handleEditEmail = async (e: React.FormEvent) => {
