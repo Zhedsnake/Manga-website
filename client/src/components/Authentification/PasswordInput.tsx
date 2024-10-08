d4cbb8b3a0f4ef0ecfdff7a743e63168532ce784
Zhedsnake <besedinzena5@gmail.com>
2024-10-08 14:31:07 +0300

    Исправление и импорт redux toolkit в PasswordInput

--- client/src/components/Authentification/PasswordInput.tsx
+++ client/src/components/Authentification/PasswordInput.tsx
@@ -10,9 +10,9 @@ import ErrorForm from "../UI/errorForm/ErrorForm";
 import PasswordToggleButton from "../UI/passwordToggleButton/PasswordToggleButton";
 import PasswordContainer from "../UI/passwordContainer/PasswordContainer";
 import {AuthContext} from "../../contexts/AuthContext.ts";
-import {useTypedSelector} from "../../../../buffer/client/src/hooks/reduxHooks/useTypedSelector.ts";
-import {useActions} from "../../../../buffer/client/src/hooks/reduxHooks/useActions.ts";
 import useInput from "../../hooks/useInput.ts";
+import {useAppSelector} from "../../hooks/reduxHooks-toolkit/useRedux.ts";
+import {setPassword} from "../../store/reducers/authForm/authFormSlice";
 
 const PasswordInput: React.FC = () => {
     const {
@@ -24,12 +24,14 @@ const PasswordInput: React.FC = () => {
 
     const [passwordErrorForm, setPasswordErrorForm] = useState('');
 
-    const { passwordError } = useTypedSelector(state => state.authFormError);
-    const { password } = useTypedSelector(state => state.authForm);
-    const {setPasswordAction} = useActions()
+    // const { passwordError } = useTypedSelector(state => state.authFormError);
+    // const { password } = useTypedSelector(state => state.authForm);
+    // const {setPasswordAction} = useActions()
+    const { passwordError } = useAppSelector(state => state.authFormError);
+    const { password } = useAppSelector(state => state.authForm);
 
     useEffect(() => {
-        setPasswordAction(passwordForm.value)
+        setPassword(passwordForm.value)
     }, [passwordForm.value])
 
     useEffect(() => {
