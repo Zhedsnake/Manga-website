bd9cada1f84b75cd4770a6aa61975912faa60f34
Zhedsnake <besedinzena5@gmail.com>
2024-10-08 14:30:41 +0300

    Исправление и импорт redux toolkit в NameInput

--- client/src/components/Authentification/NameInput.tsx
+++ client/src/components/Authentification/NameInput.tsx
@@ -4,21 +4,23 @@ import InputAuth from "../UI/inputAuth/InputAuth";
 import Label from "../UI/label/label";
 import FormGroupDiv from "../UI/formGroupdiv/formGroupdiv";
 import ErrorForm from "../UI/errorForm/ErrorForm";
-import {useActions} from "../../../../buffer/client/src/hooks/reduxHooks/useActions.ts";
-import {useTypedSelector} from "../../../../buffer/client/src/hooks/reduxHooks/useTypedSelector.ts";
 import useInput from "../../hooks/useInput.ts";
+import {setName} from "../../store/reducers/authForm/authFormSlice";
+import {useAppSelector} from "../../hooks/reduxHooks-toolkit/useRedux.ts";
 
 const NameInput = () => {
     const nameForm = useInput("")
 
     const [nameErrorForm, setNameErrorForm] = useState<string>('');
 
-    const { nameError } = useTypedSelector(state => state.authFormError);
-    const { name } = useTypedSelector(state => state.authForm);
-    const {setNameAction} = useActions()
+    // const { nameError } = useTypedSelector(state => state.authFormError);
+    // const { name } = useTypedSelector(state => state.authForm);
+    const { nameError } = useAppSelector(state => state.authFormError);
+    const { name } = useAppSelector(state => state.authForm);
+    // const {setNameAction} = useActions()
 
     useEffect(() => {
-        setNameAction(nameForm.value)
+        setName(nameForm.value)
     }, [nameForm.value])
 
     useEffect(() => {
